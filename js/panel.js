385f42de444737ea62fa77ac08c5ba104872d6e3
Катерина <katena.2002@mail.ru>
2023-03-14 21:22:38 +0300

    files

--- js/panel.js
+++ js/panel.js
@@ -3,5 +3,20 @@ const addProduct = document.querySelector('input[type=submit]')
 
 addProduct.addEventListener('click',(Event)=>{
     Event.preventDefault()
+    //product - id  ф теге form
+    let data = new FormData(product)
+
+    
+    //посмотреть все поля объекта data
+    //data.entries() - Возвращает массив 
+    for (let [k,v] of data.entries()){
+       // console.log(k,v)
+    }
+    //return false
+    fetch('hendler.php',{
+        method:'POST',
+        body: data
+    })
+    .then(response=>response.text())
     
 })
